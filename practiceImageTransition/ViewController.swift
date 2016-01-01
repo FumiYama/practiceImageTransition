ad972190d320c280c50a8a390ea0078671545077
FumiYama <f.yamanaka1127@gmail.com>
2016-01-01 20:28:18 +0900

    FirstCommit Dogs57

--- practiceImageTransition/ViewController.swift
+++ practiceImageTransition/ViewController.swift
@@ -8,11 +8,42 @@
 
 import UIKit
 
+
+extension UIImage {
+    
+    // Resizeのクラスメソッドを作る.
+    class func ResizeUIImage(image : UIImage,width : CGFloat, height : CGFloat)-> UIImage!{
+        
+        // 指定された画像の大きさのコンテキストを用意.
+        UIGraphicsBeginImageContext(CGSizeMake(width, height))
+        
+        // コンテキストに画像を描画する.
+        image.drawInRect(CGRectMake(0, 0, width, height))
+        
+        // コンテキストからUIImageを作る.
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        // コンテキストを閉じる.
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let myImage:UIImage = UIImage(named: "sample.jpg")! //画像の設定
+        
+        let myImageView = UIImageView(image: UIImage.ResizeUIImage(myImage, width: self.view.frame.width, height: self.view.frame.height)) // リサイズされたUIImageを指定して、UIImageViewを作る.
+                
+        myImageView.alpha = 0.35 //透過率
+        
+        self.view.addSubview(myImageView) //viewにUIImageViewを追加
+        
     }
 
     override func didReceiveMemoryWarning() {
